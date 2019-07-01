8e5c5bbee29a272c18c771ef86647b77113e6e6a
Jason <jlu787@outlook.com>
2019-07-01 14:21:45 +1200

    Added drag and drop and basic raccoon AI
    TestScene

---
+++ PlumJam2/Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion newQuat = new Quaternion();
+        newQuat = transform.rotation;
+        newQuat.z = 0;
+        transform.rotation = newQuat;
+	}
+}
